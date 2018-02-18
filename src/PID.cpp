0f31a2766ae27f47334ec0bf77dd294801e40ff9
Feabhas Delegate <alan.xuefei@googlemail.com>
2018-02-18 05:43:50 +0000

    implement PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <assert.h>
 
 using namespace std;
 
@@ -6,16 +7,46 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
-
-PID::~PID() {}
-
-void PID::Init(double Kp, double Ki, double Kd) {
+PID::PID()
+: p_error(0)
+, i_error(0)
+, d_error(0)
+, Kp(0)
+, Ki(0)
+, Kd(0)
+, prev_cte(0)
+{
 }
 
-void PID::UpdateError(double cte) {
+PID::~PID()
+{
 }
 
-double PID::TotalError() {
+void PID::Init(double Kp_, double Ki_, double Kd_)
+{
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+  p_error = 0;
+  d_error = 0;
+  i_error = 0;
+
+  prev_cte = 0;
+
 }
 
+void PID::UpdateError(double cte)
+{
+  
+  p_error = cte;
+  i_error += cte;
+  d_error = cte - prev_cte;
+
+  prev_cte = cte;
+}
+
+double PID::TotalError()
+{
+  return -Kp * p_error - Ki * i_error - Kd * d_error;
+}
